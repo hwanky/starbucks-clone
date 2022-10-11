9527624f3a2de1920b8fe604ebac3006b237f913
hwanky <choijh0925@gmail.com>
2022-10-11 16:08:59 +0900

    feat: Add and styling badge images & apply js libraries
    Apply lodash.js and gsap libraries on badges.

--- js/main.js
+++ js/main.js
@@ -14,3 +14,28 @@ searchInputEl.addEventListener("blur", function () {
   searchEl.classList.remove("focused");
   searchInputEl.setAttribute("placeholder", "");
 });
+
+const badgeEl = document.querySelector("header .badges");
+
+// lodash.js libraries & gsap libraries
+// _.throttle(함수, 시간) gsap.to(요소, 지속시간, 옵션)
+window.addEventListener(
+  "scroll",
+  _.throttle(function () {
+    if (window.scrollY > 500) {
+      // 배지 숨기기
+      // badgeEl.style.display = "none";
+      gsap.to(badgeEl, 0.6, {
+        opacity: 0,
+        display: "none",
+      });
+    } else {
+      // 배지 보이기
+      // badgeEl.style.display = "block";
+      gsap.to(badgeEl, 0.6, {
+        opacity: 1,
+        display: "block",
+      });
+    }
+  }, 300)
+);
